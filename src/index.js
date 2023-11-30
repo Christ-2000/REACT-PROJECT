aa82ed088225ceb9abd58a6286f3f9e29e810cee
Christ-2000 <christellamunezero91@gmail.com>
2023-11-30 02:29:48 -0800

    add anather code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react"
 import ReactDom from "react-dom/client";
-
 function Greeting (){
     return(
-    <div>(<h2>My First Component</h2>
-    <h3>My second Component</h3>
-    </div>
-
+        <div>
+        <Person/>
+        <Message/>
+        </div>
     )
 }
+const Person = () => <h2>cloude nyandwi</h2>
+const Message =() => {
+    return(
+        <div>
+            (<h2>My First Component</h2>
+    <h3>My second Component</h3>
+            <h2>kelly nyandwi</h2>
+            <h2>raul nyandwi</h2>
+        </div>
+    )
+
+}
+    
+
+    
+
 
 const root =ReactDom.createRoot(document.getElementById('root'));
 
