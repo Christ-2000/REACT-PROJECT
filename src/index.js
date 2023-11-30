eb0586fa12be350e713ec8bd59000e7481de6caf
Christ-2000 <christellamunezero91@gmail.com>
2023-11-30 02:08:52 -0800

    add new  line

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react"
 import ReactDom from "react-dom/client";
 
 function Greeting (){
-    return<h2>My First Component</h2>
+    return(
+    <div>(<h2>My First Component</h2>
+    <h3>My second Component</h3>
+    </div>
+
+    )
 }
 
 const root =ReactDom.createRoot(document.getElementById('root'));
