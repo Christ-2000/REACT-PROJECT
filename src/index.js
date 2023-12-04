2d6c698397fa6eda845a347fb8daabcc1cfbea30
Christ-2000 <christellamunezero91@gmail.com>
2023-12-04 06:31:03 -0800

    add new code

--- src/index.js
+++ src/index.js
@@ -4,16 +4,71 @@ import ReactDom from "react-dom/client";
 import './index.css'
 
 const firstBook = {
+author : "mukesharugamba nina",
+title:'job developer',
+img :'./image/amzonp1.jpg',
+id:1,
+}
+const secondBook ={
+  author:"chistella munezero",
+  title:"fullstack",
+  img:"./image/amzonp2.jpg",
+  id:2,
+}
+const thirdBook ={
+  author:"kiki",
+  title:"frontend",
+  img:"./image/amzon p.jpg",
+  id:3,
+
+}
+const fourBook ={
+  author:"nene",
+  title:"frontend",
+  img:"./image/amzonp1.jpg",
+  id:3,
 
 }
 
+
  function BookList (){
     return<section className = "booklist">
-         <Book  author={author} title= {title} img={img}/>
-         <Book  author={author} title= {title} img={img}/>
-        <Book   author={author} title= {title} img={img}/>
+         <Book  
+         author={firstBook.author} 
+         title= {firstBook.title} 
+         img={firstBook.img}>
+         <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
+          repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
+        </p>
+        <button class="btn">click me</button>
+        </Book>
+         <Book  author={secondBook.author} title= {secondBook.title} img={secondBook.img}>
+         <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
+          repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
+        </p>
+        <button class="btn">click me</button>
+        </Book>
+        <Book   author={thirdBook.author} title= {thirdBook.title} img={thirdBook.img}>
+        <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
+        repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
+      </p>
+      <button class="btn">click me</button>
+      </Book>
+      <Book   author={fourBook.author} title= {fourBook.title} img={fourBook.img}>
+        <h5>hello every one I Like code with my heart</h5>
+      </Book>
          </section>
  }
+
+ const names =['john','peter','kiki']
+
+
+
+
+
  const img = "./image/amzonp2.jpg"
  const title = "Interesting Facts For Curious Minds"
  const author = "christella MUNEZERO"
@@ -25,12 +80,14 @@ const firstBook = {
 //     </article>
 // }
 const Book = (props) => {
-    console.log(props)
+  const {image,title,author,children,button} =props
     return(
     <article className = "book">
         <img src={props.img} alt ={props.title}/>
             <h2>{props.title}</h2>
             <h4>{props.author}</h4>
+            {children}
+            {button}
     </article>
     )
 }
